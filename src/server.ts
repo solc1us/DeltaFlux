702fb41d0efdb2d29c6f0fb74d82e3ce9eb8487d
solc1us <cyan.magina@gmail.com>
2026-03-19 17:48:30 +0700

    refactor: server.ts added time

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 // src/server.ts
-import app from './app';
-import { env } from './config/env';
+import app from "./app";
+import { env } from "./config/env";
 
 app.listen(env.port, () => {
-  console.log(`Server running on port ${env.port} in ${env.nodeEnv} mode`);
+	console.log(
+		`[${new Date().toLocaleTimeString()}] Server running on port ${env.port} in ${env.nodeEnv} mode`,
+	);
 });
